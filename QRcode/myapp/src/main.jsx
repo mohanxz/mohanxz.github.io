8a94ae238f6ae84793e2b1dce1b0678a209f0456
mohanxz <mohanrajs2k04@gmail.com>
2024-11-23 08:56:33 +0530

    Qrcode Generator uploaded

---
+++ QRcode/myapp/src/main.jsx
@@ -0,0 +1,12 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import './QrCode.css'
+import { QrCode } from './QrCode'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <QrCode/>
+    {/* <App /> */}
+  </StrictMode>,
+)
