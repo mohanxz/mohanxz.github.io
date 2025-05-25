69a082e07db5c542cf0426b80b8c438514ea0816
mohanxz <mohanrajs2k04@gmail.com>
2025-05-25 21:40:08 +0530

    front end dev to full stack dev

--- js/main.js
+++ js/main.js
@@ -46,7 +46,6 @@ function applyMode(mode){
     toggle.style.backgroundImage='url(/images/pexels-arnie-chou-304906-1229042.jpg)'
     toggle.style.backgroundPosition=''
     indicator.style.transition='0.5s';
-    me.style.backgroundImage='url(/images/mortext.jpg)'
     navbar.style.backgroundImage='linear-gradient(315deg, #130f40 0%, #000000 74%),url(/images/sidebars.jpeg)';
     myself.style.borderColor='#deff58'
     
@@ -62,7 +61,6 @@ function applyMode(mode){
   else{
     body.classList.replace('dark','light');
     toggle.classList.add('active');
-    toggle.style.backgroundImage='url(/images/morsky.jpg)'
     toggle.style.backgroundSize='cover';
     toggle.style.filter='brightness(1.5)'
     me.style.backgroundImage='url(/images/wakdn.webp)';
